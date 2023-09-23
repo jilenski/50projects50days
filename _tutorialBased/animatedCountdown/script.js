7ef63b52ae28c81e40ecf575e143da32c9f94c4e
jilenski <jtayhopon@gmail.com>
2023-09-23 17:05:12 +0800

    update script for animation reset & replay button

--- _tutorialBased/animatedCountdown/script.js
+++ _tutorialBased/animatedCountdown/script.js
@@ -7,6 +7,17 @@ console.log(nums);
 
 runAnimation();
 
+function resetDOM() {
+  counter.classList.remove('hide');
+  finalMessage.classList.remove('show');
+
+  nums.forEach((num) => {
+    num.classList.value = '';
+  });
+
+  nums[0].classList.add('in');
+}
+
 function runAnimation() {
   nums.forEach((num, idx) => {
     const nextToLast = nums.length - 1;
@@ -16,7 +27,7 @@ function runAnimation() {
         num.classList.remove('in');
         num.classList.add('out');
       } else if (e.animationName === 'goOut' && num.nextElementSibling) {
-        num.nextElementSibling.classlist.add('in');
+        num.nextElementSibling.classList.add('in');
       } else {
         counter.classList.add('hide');
         finalMessage.classList.add('show');
@@ -24,3 +35,8 @@ function runAnimation() {
     });
   });
 }
+
+replay.addEventListener('click', () => {
+  resetDOM();
+  runAnimation();
+});
