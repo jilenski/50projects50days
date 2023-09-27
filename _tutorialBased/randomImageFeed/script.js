9cb67c9907be36ce5227cea339fc064e40d17bfb
jilenski <jtayhopon@gmail.com>
2023-09-27 11:49:41 +0800

    update stylesheet

--- _tutorialBased/randomImageFeed/script.js
+++ _tutorialBased/randomImageFeed/script.js
@@ -1,6 +1,6 @@
 const container = document.querySelector('.container');
 const unsplashURL = 'https://source.unsplash.com/random/';
-const rows = 10;
+const rows = 5;
 
 for (let i = 0; i < rows * 3; i++) {
   const img = document.createElement('img');
@@ -15,3 +15,4 @@ function getRandomSize() {
 function getRandomNr() {
   return Math.floor(Math.random() * 10) + 300;
 }
+      
